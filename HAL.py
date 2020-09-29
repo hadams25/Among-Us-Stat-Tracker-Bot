06867ac14875eed413550cc74ddf599440bd6ad2
hadams25 <46791347+hadams25@users.noreply.github.com>
2020-09-28 23:51:26 -0500

    Change database software

--- HAL.py
+++ HAL.py
@@ -1,6 +1,6 @@
 import discord
 import json
-import sqlite3
+import psycopg2
 import os.path
 import sys
 from sqlite3 import Error
@@ -11,25 +11,18 @@ settings = {}
 default_settings = {
     "token" : "",
     "status" : "",
-    "prefix" : "~"
+    "prefix" : "~",
+    "host" : "localhost",
+    "database" : "player_data",
+    "user" : "postgres",
+    "password" : "",
+    "port" : "5432"
 }
-
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
 
 def start():
     #if the "Servers" directory, doesn't exist, create one
-    if not path.exists("Servers"):
-        print("Server directory not found. One will be created.")
+    if not path.exists("player_data.db"):
+        print("Player database not found. One will be created.")
         os.mkdir("Servers")
 
     #if the settings json file does not exist, create one
@@ -56,4 +49,27 @@ def start():
     
     client.run(settings["token"])
 
-#if __name__ == '__main__':
+def database_check():
+
+@client.event
+async def on_ready():
+    print('Logged in as ' + client.user.name)
+    print('------')
+    game = discord.Game(settings["status"])
+    await client.change_presence(status=discord.Status.idle, activity=game)
+    print("Currently serving the following guilds:")
+    for server in client.guilds:
+        print("- " + server.name)
+
+@client.event
+async def on_guild_join(guild):
+    original_dir = os.getcwd()
+    os.chdir(original_dir + "/Servers")
+    if not path.exists(guild.id + " - " + guild.name + ".db"):
+        create_connection(guild.id + " - " + guild.name + ".db")
+
+
+
+
+if __name__ == '__main__':
+    start()
