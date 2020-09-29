99e065c72d72e5d4b1a2861815061ba2c96ad76e
hadams25 <46791347+hadams25@users.noreply.github.com>
2020-09-28 22:04:23 -0500

    Add startup code to bot

--- HAL.py
+++ HAL.py
@@ -1,7 +1,18 @@
 import discord
 import json
 import sqlite3
+import os.path
+import sys
 from sqlite3 import Error
+from os import path
+
+client = discord.Client()
+settings = {}
+default_settings = {
+    "token" : "",
+    "status" : "",
+    "prefix" : "~"
+}
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -15,6 +26,34 @@ def create_connection(db_file):
         if conn:
             conn.close()
 
+def start():
+    #if the "Servers" directory, doesn't exist, create one
+    if not path.exists("Servers"):
+        print("Server directory not found. One will be created.")
+        os.mkdir("Servers")
 
-if __name__ == '__main__':
-    create_connection("pythonsqlite.db")
+    #if the settings json file does not exist, create one
+    if not path.exists("settings.json"):
+        print("Settings file not found. One will be created.")
+
+        with open("settings.json", "w") as j_file:
+            json.dump(default_settings, j_file)
+    
+    with open("settings.json") as j_file:
+        settings = json.load(j_file)
+
+    #if settings from file are missing fields, add them from the default settings
+    for key in default_settings.keys():
+        if not key in settings:
+            settings[key] = default_settings[key]
+    
+    #check if the token field is empty
+    if settings["token"] == "":
+        print("Token field is empty in settings.json")
+        print("Please supply a discord bot token.")
+        print("Shutting down...")
+        sys.exit()
+    
+    client.run(settings["token"])
+
+#if __name__ == '__main__':
